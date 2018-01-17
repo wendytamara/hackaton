91b97c931a706c18e4cbbf3ec33de9dfa7847e44
dayamll <dayamllerenacastro@gmail.com>
2018-01-17 18:34:36 -0500

    Ordenando archivos

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-/* NAVBAR*/
-$('.button-collapse').sideNav();
-$(document).ready(function() {
-  $('.dropdown-button').dropdown();
-});
-
-/* Carrusel*/
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-$('.carousel').carousel();
-setInterval(function() {
-  $('.carousel').carousel('next');
-}, 5000); // every 2 seconds
-
-/*Select*/
-
-$(document).ready(function() {
-  $('select').material_select();
-});
-
-/*Modales */
-
-$(document).ready(function(){
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-});
-       
 $(document).ready(function () {
   var config = {
     apiKey: 'AIzaSyCTEIYIpKwepybwx0Gpfh7uKVs339oAZn8',
