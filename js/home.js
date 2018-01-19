d9194a83855245668d301d28c4effd1eafdffd7c
eleyne <eleynekrd@gmail.com>
2018-01-19 06:50:40 -0600

    Agregando filtros

--- js/home.js
+++ js/home.js
@@ -25,3 +25,22 @@ $(document).ready(function() {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
+
+$(document).ready(function() {
+ 
+  // Filtros del input
+  $('.search').keyup(function() {
+    var name = $(this).val().toLowerCase();
+    $('.collection').hide();
+   
+    $('.collection').each(function() { // filtro por titulo
+      var search = $(this).text();
+      var $title = $(this).data('title');
+      if ($title.indexOf(name) !== -1) {
+        $(this).show();
+      }
+    });
+   
+  });
+});
+
