3c4eddce0b276b8697d8a70cffa43083c9f92e8f
Danielapariona <daniela.parionac@gmail.com>
2018-01-19 01:39:07 -0500

    Detalles de la película

--- js/home.js
+++ js/home.js
@@ -15,13 +15,13 @@ setInterval(function () {
 
 /* Select*/
 
-$(document).ready(function () {
+$(document).ready(function() {
   $('select').material_select();
 });
 
 /* Modales */
 
-$(document).ready(function () {
+$(document).ready(function() {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
